8fe2a760ee1c64fa4b2d2d24b38441edc3970958
javier923 <ramirezjavier5559@gmail.com>
2020-04-02 01:27:11 -0600

    unidad 5

---
+++ UNIDAD 5/UNIDAD_5/ARREGLOS/Arreglos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARREGLOS
+{
+    class Arreglos
+    {
+        //main principal
+        static void Main(string[] args)
+        {
+            Arreglos CAL = new Arreglos();
+            CAL.Cargar();
+            CAL .Imprimir();
+        }
+
+        private int[] calificaciones; //Declaramos un arreglo
+
+        public void Cargar()
+        {
+            calificaciones = new int[6]; //Inicializamos el arreglo en 5
+            for (int f = 1; f <= 5; f++)
+            {
+                Console.Write("Ingrese calificacion " + f + ": ");
+                string linea;
+                linea = Console.ReadLine();
+                calificaciones[f] = int.Parse(linea); //Asignamos las 5 calificaciones al arreglo
+            }
+        }
+        //Muestra las calificaciones de los alumnos en el arreglo  calificaciones[f]
+        public void Imprimir()
+        {
+            Console.Write("Las 5 calificaciones de los alumnos  \n");
+            for (int f = 1; f <= 5; f++)
+            {
+                Console.Write("[" + calificaciones[f] + "] ");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
+
